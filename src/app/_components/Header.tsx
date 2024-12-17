f3ec3934aad84fd9e472f34f43ad03e73a7fb135
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-12-17 19:06:07 +0545

    Add Clerk authentication; implement Dashboard and Sign Up pages, and update Header component

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,12 +1,28 @@
+"use client";
 import Image from 'next/image'
 import logo from '@public/assets/logo.svg'
 import { Button } from '@/components/ui/button'
+import { UserButton, useUser } from '@clerk/nextjs'
+import Link from 'next/link'
 
 function Header() {
+
+    const { isSignedIn } = useUser();
+
     return (
         <div className='p-5 flex justify-between items-center border shadow-sm'>
-            <Image src={logo} alt="logo" width={160} height={100} />
-            <Button>Get started</Button>
+            <Link href={
+                isSignedIn ? '/dashboard' : '/'
+            }>
+                <Image src={logo} alt="logo" width={160} height={100} />
+            </Link>
+            {
+                isSignedIn
+                    ? <UserButton /> :
+                    <Link href="/sign-in">
+                        <Button>Sign In</Button>
+                    </Link>
+            }
         </div>
     )
 }
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../_components/Header'
+
+const Dashboard = () => {
+    return (
+        <>
+            <Header />
+            <div>
+                This is Dashboard page
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
