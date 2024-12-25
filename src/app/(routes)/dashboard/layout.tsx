8b009cf3b7587457c8070395f668db744324e0a2
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-12-25 14:02:07 +0545

    Add user budget check in DashboardLayout; redirect to budget page if no budget found

--- src/app/(routes)/dashboard/layout.tsx
+++ src/app/(routes)/dashboard/layout.tsx
@@ -1,8 +1,36 @@
-import { ReactNode } from 'react';
+"use client"
+import { ReactNode, useEffect } from 'react';
 import SideNavbar from './_components/SideNavbar';
 import DashboardHeader from './_components/DashboardHeader';
+import { useUser } from '@clerk/nextjs';
+import { db } from '../../../db/index';
+import { Budgets } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+import { useRouter } from 'next/navigation';
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
+    const { user } = useUser();
+    const router = useRouter();
+
+    const checkUserBudget = async () => {
+        if (user?.primaryEmailAddress?.emailAddress) {
+            const result = await db.select()
+                .from(Budgets)
+                .where(eq(Budgets.createdBy, user.primaryEmailAddress.emailAddress))
+            if (result?.length === 0) {
+                router.replace('/dashboard/budget')
+            }
+
+        }
+
+    }
+    useEffect(() => {
+        if (user) {
+            checkUserBudget();
+        }
+    },[user])
+
+
     return (
         <>
             <div className="fixed md:w-64 hidden md:block "><SideNavbar /></div>
