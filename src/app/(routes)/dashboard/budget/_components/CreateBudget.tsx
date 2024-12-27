cccc299e194d27564d6f809e62c803309e420a69
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-12-27 20:39:54 +0545

    Add emoji picker to CreateBudget component; implement dialog for budget creation and update package.json with new dependencies

--- src/app/(routes)/dashboard/budget/_components/CreateBudget.tsx
+++ src/app/(routes)/dashboard/budget/_components/CreateBudget.tsx
@@ -1,12 +1,56 @@
-import React from 'react'
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import EmojiPicker from "emoji-picker-react"
+import { useState } from "react"
 
 const CreateBudget = () => {
+    const [emojiIcon, setEmojiIcon] = useState('🙂')
+    const [openEmojiPicker, setOpenEmojiPicker] = useState(false)
     return (
         <>
             <div className="bg-slate-300 p-10 rounded-md hover:shadow-md flex flex-col items-center justify-center space-x-4 border-2 border-dashed cursor-pointer">
-                <h2 className='text-3xl'>+</h2>
-                <h2 className='font-semibold'>Create New Budget</h2>
-            </div>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <div className="flex flex-col items-center justify-center space-x-4">
+                            <h2 className='text-3xl'>+</h2>
+                            <h2 className='font-semibold'>Create New Budget</h2>
+                        </div>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>
+                                Create New Budget
+                            </DialogTitle>
+                            <DialogDescription asChild>
+                                <div className="mt-5">
+                                    <Button variant={"outline"}
+                                        className="text-lg"
+                                    onClick={() => setOpenEmojiPicker(!openEmojiPicker)}
+                                    >{emojiIcon}</Button>
+                                <div className="mt-3 absolute">
+                                    <EmojiPicker
+                                        open={openEmojiPicker}
+                                        onEmojiClick={(emojiObject) => {
+                                            setEmojiIcon(emojiObject.emoji)
+                                            setOpenEmojiPicker(false)
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+
+        </div >
         </>
     )
 }
