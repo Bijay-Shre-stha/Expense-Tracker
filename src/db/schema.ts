7908dc1cba58c7e8f981d190cabd788ed96dbf89
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-12-31 20:00:52 +0545

    Add Expenses table to schema with foreign key reference to Budgets; define Expense type for inference

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import {  pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+import {  integer, pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const Budgets = pgTable('budgets', {
     id:serial('id').primaryKey(),
@@ -9,4 +9,13 @@ export const Budgets = pgTable('budgets', {
     createdAt: timestamp('created_at').notNull().defaultNow(),
 })
 
+export const Expenses = pgTable('expenses', {
+    id:serial('id').primaryKey(),
+    name:varchar('name').notNull(),
+    amount:varchar('amount').notNull().default('0'),
+    budgetId:integer('budget_id').references(()=>Budgets.id),
+    createdAt: varchar('created_at').notNull(),
+})
+
 export type Budget = typeof  Budgets.$inferInsert;
+export type Expense = typeof  Expenses.$inferInsert;
