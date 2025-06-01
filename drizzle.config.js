8fc065995e7ff86410ebada8ca2e3c76cb65c764
Bijay Shrestha <bijayashrestha929@gmail.com>
2025-06-01 18:01:03 +0545

    Remove database configuration file for security and cleanup

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-  schema: "./utils/schema.jsx",
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: "postgresql://neondb_owner:kTQWNzu84gVF@ep-floral-brook-a5x3vv69.us-east-2.aws.neon.tech/Expense-Tracker?sslmode=require",
-  }
-};
